901c3fb81a77d0f630c0dc53f9c8cc1856f6e5e0
Rohini <rvaidya@deloitte.com>
2023-05-30 22:18:56 +0530

    Initial Commit

---
+++ CameraFilters/CameraFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraFiltersApp.swift
+//  CameraFilters
+//
+//  Created by rvaidya on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
